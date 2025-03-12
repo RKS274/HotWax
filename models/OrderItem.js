d93fc40491506feac05037daaa981dddc4a15226
Lokesh Kumar Goyal <152732164+RKS274@users.noreply.github.com>
2025-03-12 19:04:57 +0530

    Create OrderItem.js

---
+++ models/OrderItem.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const OrderItemSchema = new mongoose.Schema({
+  order: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'OrderHeader',
+    required: true
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+    default: 'Pending'
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('OrderItem', OrderItemSchema);
